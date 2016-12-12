a6ea2ce07b244b8286c76688a485bfda62493c6e
Baskarant <baskar159@gmail.com>
2016-12-13 01:08:24 +0530

    code 2.0 13/12/2016 1.08 a.m

--- imagePickerExample/imagePickerExample/ViewController.swift
+++ imagePickerExample/imagePickerExample/ViewController.swift
@@ -8,20 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UIImagePickerControllerDelegate ,UINavigationControllerDelegate {
+    
     @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var cameraBtn: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        cameraBtn.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
     }
 
     @IBAction func pickImage(_ sender: AnyObject) {
-        
-        let pickerController = UIImagePickerController()
-        self.present(pickerController, animated: true, completion: nil)
+       /* let pickerController = UIImagePickerController()
+        self.present(pickerController, animated: true, completion: nil) */
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imagePickerView.contentMode = .scaleAspectFit
+            imagePickerView.image = image
+            dismiss(animated: true, completion: nil)
+        }
     }
 
+    @IBAction func pickFromCamera(_ sender: AnyObject) {
+        let imageCpicker = UIImagePickerController()
+        imageCpicker.delegate = self
+        imageCpicker.sourceType = .camera
+        present(imageCpicker, animated: true, completion: nil)
+    }
+    
+    
+    
 }
 
